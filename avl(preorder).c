195edf8d1f9122bdcf8aab50ddd6a8101aeeddba
Shreya Pulla <154301275+Shreya5i4@users.noreply.github.com>
2024-09-30 10:05:50 +0530

    Add files via upload

---
+++ avl(preorder).c
@@ -0,0 +1,127 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+// Structure of each node
+struct Node {
+int key;
+struct Node *left;
+struct Node *right;
+int height;
+};
+// A utility function to get the maximum of two integers
+int max(int a, int b) {
+return (a > b) ? a : b;
+}
+// A utility function to get the height of the tree
+int height(struct Node *N) {
+if (N == NULL)
+return 0;
+return N->height;
+}
+// A utility function to create a new node
+struct Node* newNode(int key) {
+struct Node* node = (struct Node*)
+malloc(sizeof(struct Node));
+node->key = key;
+node->left = NULL;
+node->right = NULL;
+node->height = 1; // New node is initially added at leaf
+return(node);
+}
+// A utility function to right rotate subtree rooted with y
+struct Node *rightRotate(struct Node *y) {
+struct Node *x = y->left;
+struct Node *T2 = x->right;
+// Perform rotation
+x->right = y;
+y->left = T2;
+// Update heights
+y->height = max(height(y->left), height(y->right)) + 1;
+x->height = max(height(x->left), height(x->right)) + 1;
+// Return new root
+return x;
+}
+// A utility function to left rotate subtree rooted with x
+struct Node *leftRotate(struct Node *x) {
+struct Node *y = x->right;
+struct Node *T2 = y->left;
+
+// Perform rotation
+y->left = x;
+x->right = T2;
+// Update heights
+x->height = max(height(x->left), height(x->right)) + 1;
+y->height = max(height(y->left), height(y->right)) + 1;
+// Return new root
+return y;
+}
+// Get Balance factor of node N
+int getBalance(struct Node *N) {
+if (N == NULL)
+return 0;
+return height(N->left) - height(N->right);
+}
+// Recursive function to insert a key in the subtree rooted with node and returns the new root of the
+//subtree.
+struct Node* insertNode(struct Node* node, int key) {
+// Perform the normal BST insertion
+if (node == NULL)
+return(newNode(key));
+if (key < node->key)
+node->left = insertNode(node->left, key);
+else if (key > node->key)
+node->right = insertNode(node->right, key);
+else
+return node; // Equal keys are not allowed in BST
+// Update height of this ancestor node
+node->height = 1 + max(height(node->left), height(node->right));
+// Get the balance factor of this ancestor node to check whether this node became unbalanced
+int balance = getBalance(node);
+// If this node becomes unbalanced, then there are 4 cases
+// Left Left Case
+if (balance > 1 && key < node->left->key)
+return rightRotate(node);
+// Right Right Case
+if (balance < -1 && key > node->right->key)
+return leftRotate(node);
+// Left Right Case
+if (balance > 1 && key > node->left->key) {
+node->left = leftRotate(node->left);
+return rightRotate(node);
+}
+
+// Right Left Case
+if (balance < -1 && key < node->right->key) {
+node->right = rightRotate(node->right);
+return leftRotate(node);
+}
+// Return the (unchanged) node pointer
+return node;
+}
+// A utility function to print preorder traversal of the tree.
+void printPreOrder(struct Node *root) {
+if (root != NULL) {
+printf("%d ", root->key);
+printPreOrder(root->left);
+printPreOrder(root->right);
+}
+}
+int main() {
+struct Node *root = NULL;
+/* Constructing tree */
+root = insertNode(root, 10);
+root = insertNode(root, 20);
+root = insertNode(root, 30);
+root = insertNode(root, 40);
+root = insertNode(root, 50);
+root = insertNode(root, 25);
+// Preorder Traversal
+printPreOrder(root);
+return 0;
+}
